55becca07cca2074f53cc9cc8cf1bc6d9525c20b
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-06-11 21:53:41 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origin=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
